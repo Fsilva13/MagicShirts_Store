ad2181ab4c06e26fea5c1c9edd8154adcdd925d3
Igor Paulo <ipaulo@example.com>
2021-05-17 16:54:24 +0100

    form ncomendas
    form

--- app/Encomenda.php
+++ app/Encomenda.php
@@ -6,18 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Encomenda extends Model
 {
-<<<<<<< Updated upstream
-		protected $fillable = [
-        'estado', 'notas', 'nif','endereco','metpag',
-    ];
 
-=======
 	protected $fillable = [
         'estado', 'notas', 'nif','endereco','metpag',
     ];
 
-
->>>>>>> Stashed changes
     public function clientes(){
       return $this->belongsTo('App\Cliente');
     }
