b2de7d911b1008d1ce9c7795dd2f97ad93f9af9f
Filipe Silva <filipesilva1315@gmail.com>
2021-06-20 17:39:35 +0100

    20/06

--- resources/views/pdf/pdf_view.blade.php
+++ resources/views/pdf/pdf_view.blade.php
@@ -1,187 +1,85 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8" />
-		<title>A simple, clean, and responsive HTML invoice template</title>
 
-		<style>
-			.invoice-box {
-				max-width: 800px;
-				margin: auto;
-				padding: 30px;
-				border: 1px solid #eee;
-				box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
-				font-size: 16px;
-				line-height: 24px;
-				font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
-				color: #555;
-			}
+<head>
+  <meta charset="utf-8" />
 
-			.invoice-box table {
-				width: 100%;
-				line-height: inherit;
-				text-align: left;
-			}
+  <link href="{{ asset('css/pdf.css') }}" rel="stylesheet">
 
-			.invoice-box table td {
-				padding: 5px;
-				vertical-align: top;
-			}
+</head>
 
-			.invoice-box table tr td:nth-child(2) {
-				text-align: right;
-			}
+<body>
+  <div class="invoice-box">
+    <table cellpadding="0" cellspacing="0">
+      <tr class="top">
+        <td colspan="2">
+          <table>
+            <tr>
+              <td class="title">
+                <img src="https://getlogovector.com/wp-content/uploads/2020/12/malaysian-global-innovation-and-creativity-centre-magic-logo-vector.png" style="width: 100%; max-width: 300px" />
+              </td>
 
-			.invoice-box table tr.top table td {
-				padding-bottom: 20px;
-			}
+              <td>
+                Fatura #: {{$encomenda->id}}<br />
+                {{$encomenda->data}}<br />
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
 
-			.invoice-box table tr.top table td.title {
-				font-size: 45px;
-				line-height: 45px;
-				color: #333;
-			}
+      <tr class="information">
+        <td colspan="2">
+          <table>
+            <tr>
+              <td>
+                MagicShirts, Inc.<br />
+                12345 Sunny Road<br />
+                Sunnyville, CA 12345
+              </td>
 
-			.invoice-box table tr.information table td {
-				padding-bottom: 40px;
-			}
+              <td>
+                {{$encomenda->cliente->user->name}}<br />
+                {{$encomenda->cliente->user->email}}
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
 
-			.invoice-box table tr.heading td {
-				background: #eee;
-				border-bottom: 1px solid #ddd;
-				font-weight: bold;
-			}
+      <tr class="heading">
+        <td>Payment Method</td>
 
-			.invoice-box table tr.details td {
-				padding-bottom: 20px;
-			}
+        <td>##</td>
+      </tr>
 
-			.invoice-box table tr.item td {
-				border-bottom: 1px solid #eee;
-			}
+      <tr class="details">
+        <td>{{$encomenda->tipo_pagamento}}</td>
 
-			.invoice-box table tr.item.last td {
-				border-bottom: none;
-			}
+        <td>{{$encomenda->preco_total}} €</td>
+      </tr>
 
-			.invoice-box table tr.total td:nth-child(2) {
-				border-top: 2px solid #eee;
-				font-weight: bold;
-			}
+      <tr class="heading">
+        <td>Item</td>
 
-			@media only screen and (max-width: 600px) {
-				.invoice-box table tr.top table td {
-					width: 100%;
-					display: block;
-					text-align: center;
-				}
+        <td>Price</td>
+      </tr>
+      @foreach($encomenda->tshirts as $tshirt)
+      <tr class="item">
+        <td>Tshirt: {{$tshirt->estampa->nome}}</td>
 
-				.invoice-box table tr.information table td {
-					width: 100%;
-					display: block;
-					text-align: center;
-				}
-			}
+        <td>{{$tshirt->preco_un}} €</td>
+      </tr>
+      @endforeach
 
-			/** RTL **/
-			.invoice-box.rtl {
-				direction: rtl;
-				font-family: Tahoma, 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
-			}
 
-			.invoice-box.rtl table {
-				text-align: right;
-			}
+      <tr class="total">
+        <td></td>
 
-			.invoice-box.rtl table tr td:nth-child(2) {
-				text-align: left;
-			}
-		</style>
-	</head>
+        <td>Total: {{$encomenda->preco_total}} €</td>
+      </tr>
+    </table>
+  </div>
+</body>
 
-	<body>
-		<div class="invoice-box">
-			<table cellpadding="0" cellspacing="0">
-				<tr class="top">
-					<td colspan="2">
-						<table>
-							<tr>
-								<td class="title">
-									<img src="https://www.sparksuite.com/images/logo.png" style="width: 100%; max-width: 300px" />
-								</td>
-
-								<td>
-									Invoice #: 123<br />
-									Created: January 1, 2015<br />
-									Due: February 1, 2015
-								</td>
-							</tr>
-						</table>
-					</td>
-				</tr>
-
-				<tr class="information">
-					<td colspan="2">
-						<table>
-							<tr>
-								<td>
-									Sparksuite, Inc.<br />
-									12345 Sunny Road<br />
-									Sunnyville, CA 12345
-								</td>
-
-								<td>
-									Acme Corp.<br />
-									John Doe<br />
-									john@example.com
-								</td>
-							</tr>
-						</table>
-					</td>
-				</tr>
-
-				<tr class="heading">
-					<td>Payment Method</td>
-
-					<td>Check #</td>
-				</tr>
-
-				<tr class="details">
-					<td>Check</td>
-
-					<td>1000</td>
-				</tr>
-
-				<tr class="heading">
-					<td>Item</td>
-
-					<td>Price</td>
-				</tr>
-
-				<tr class="item">
-					<td>Website design</td>
-
-					<td>$300.00</td>
-				</tr>
-
-				<tr class="item">
-					<td>Hosting (3 months)</td>
-
-					<td>$75.00</td>
-				</tr>
-
-				<tr class="item last">
-					<td>Domain name (1 year)</td>
-
-					<td>$10.00</td>
-				</tr>
-
-				<tr class="total">
-					<td></td>
-
-					<td>Total: $385.00</td>
-				</tr>
-			</table>
-		</div>
-	</body>
 </html>
