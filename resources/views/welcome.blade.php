d578d16fe8a6dbd9bab718fd78b8f77dc3ff1c91
Filipe Silva <filipesilva1315@gmail.com>
2021-06-22 19:57:02 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
+@extends('layouts.app')
+@section('content')
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
 
     <title>Magic Shirts</title>
 
@@ -70,13 +69,13 @@
 <body>
     <div id="particles-js">
         <div class="flex-center position-ref full-height">
-
-
             <div class="content">
                 <div class="title m-b-md">
                     MagicShirts Store
                 </div>
-
+                <div>
+                    <a href="{{route('estampas.list')}}" class="btn btn-secondary"> Catalogo de Estampas</a>
+                </div>
             </div>
         </div>
     </div>
@@ -85,8 +84,10 @@
 <script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js"></script>
 
 <script>
-particlesJS.load('particles-js', 'js/package.json', function() {
-    console.log('callback - particles.js config loaded')});
-
+    particlesJS.load('particles-js', 'js/package.json', function() {
+        console.log('callback - particles.js config loaded')
+    });
 </script>
+
 </html>
+@endsection
